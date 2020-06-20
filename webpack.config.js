04833f523f2f50e176c6ae4507f4cad48810e56d
maksim-korzhov <korzhov.m.s@yandex.ru>
2020-06-20 23:57:47 +0300

    Add wepack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,26 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devMode = process.env && process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    entry: './src/index.html',
+    entry: {
+        'js/main.js': './src/index.js',
+        'css/main.css': './src/scss/main.scss' //styles
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index.html'
+        filename: '[name]'
     },
     module: {
         rules: [
             {
                 test: /\.scss$/,
+                exclude: /(node_modules)/,
                 use: [
-                    // fallback to style-loader in development
-                    process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    MiniCssExtractPlugin.loader,
                     'css-loader',
                     'sass-loader',
                 ],
@@ -24,6 +31,13 @@ module.exports = {
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: 'Output Management',
+            template: path.join(__dirname, 'src/index.html'),
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            // filename: devMode ? '[name].css' : '[name].[hash].css',
+            // chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
         }),
     ],
     devServer: {
